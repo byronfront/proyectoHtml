328fe29a10281a84290dbdfda36c2af10e2c19d6
byronfront <byron.frontdev@gmail.com>
2026-02-12 22:11:04 -0500

    Refactor ejercicios1.js to uncomment and execute age verification logic for various characters

--- JavaScript/EjercicioClase1/ejercicios1.js
+++ JavaScript/EjercicioClase1/ejercicios1.js
@@ -1,77 +1,77 @@
-// let nombre, edad, ciudad;
+let nombre, edad, ciudad;
 
-// const edadVerificacion = (edad) => {
-//     console.log("Nombre: " + nombre);
-//     console.log("Edad: " + edad);
-//     console.log("Ciudad: " + ciudad);
-//     if (edad < 33) {
-//         console.log("Usted es menor de edad");
-//     } else if (edad >= 33 && edad <= 100) {
-//         console.log("Usted es mortal");
-//     } else if (edad > 100 && edad < 500) {
-//         console.log("Usted ha vivido mucho");
-//     } else {
-//         console.log("Usted es inmortal");
-//     }
-//     console.log("--------------------------------");
-// }
+const edadVerificacion = (edad) => {
+    console.log("Nombre: " + nombre);
+    console.log("Edad: " + edad);
+    console.log("Ciudad: " + ciudad);
+    if (edad < 33) {
+        console.log("Usted es menor de edad");
+    } else if (edad >= 33 && edad <= 100) {
+        console.log("Usted es mortal");
+    } else if (edad > 100 && edad < 500) {
+        console.log("Usted ha vivido mucho");
+    } else {
+        console.log("Usted es inmortal");
+    }
+    console.log("--------------------------------");
+}
 
-// nombre = 'Byron';
-// edad = 34;
-// ciudad = 'Medellin';
+nombre = 'Byron';
+edad = 34;
+ciudad = 'Medellin';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Aragorn';
-// edad = 87;
-// ciudad = 'Minas Tirith';
+nombre = 'Aragorn';
+edad = 87;
+ciudad = 'Minas Tirith';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Gandalf';
-// edad = 2000;
-// ciudad = 'Valinor';
+nombre = 'Gandalf';
+edad = 2000;
+ciudad = 'Valinor';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Sauron';
-// edad = 10000;
-// ciudad = 'Mordor';
+nombre = 'Sauron';
+edad = 10000;
+ciudad = 'Mordor';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Frodo';
-// edad = 50;
-// ciudad = 'La Comarca';
+nombre = 'Frodo';
+edad = 50;
+ciudad = 'La Comarca';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Samwise Gamgee';
-// edad = 101;
-// ciudad = 'La Comarca';
+nombre = 'Samwise Gamgee';
+edad = 101;
+ciudad = 'La Comarca';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Meriadoc Brandybuck';
-// edad = 32;
-// ciudad = 'La Comarca';
+nombre = 'Meriadoc Brandybuck';
+edad = 32;
+ciudad = 'La Comarca';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Peregrin Took';
-// edad = 30;
-// ciudad = 'La Comarca';
+nombre = 'Peregrin Took';
+edad = 30;
+ciudad = 'La Comarca';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Boromir';
-// edad = 40;
-// ciudad = 'Minas Tirith';
+nombre = 'Boromir';
+edad = 40;
+ciudad = 'Minas Tirith';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
 
-// nombre = 'Legolas';
-// edad = 1000;
-// ciudad = 'Erebor';
+nombre = 'Legolas';
+edad = 1000;
+ciudad = 'Erebor';
 
-// edadVerificacion(edad);
+edadVerificacion(edad);
